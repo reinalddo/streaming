01e95fff2f8133e0c9cd23e5ac8789f14fe5c336
Reinaldo Matheus <reinalddo@gmail.com>
2026-04-23 13:45:13 -0400

    Base de datos Servidor y Local

--- config/database.php
+++ config/database.php
@@ -2,11 +2,31 @@
 
 declare(strict_types=1);
 
-const DB_HOST = '127.0.0.1';
 const DB_PORT = '3306';
-const DB_NAME = 'admincorreos';
-const DB_USER = 'root';
-const DB_PASS = '';
+
+function getDatabaseConfig(): array
+{
+    $host = strtolower((string) ($_SERVER['HTTP_HOST'] ?? $_SERVER['SERVER_NAME'] ?? 'localhost'));
+    $host = preg_replace('/:\d+$/', '', $host) ?? $host;
+
+    if (in_array($host, ['streaming.reborxstore.com', 'streaming.reboxstore.com'], true)) {
+        return [
+            'host' => 'srv1999.hstgr.io',
+            'port' => DB_PORT,
+            'name' => 'u680460687_admincorreos',
+            'user' => 'u680460687_admincorreos',
+            'pass' => 'n7!/$l1K#',
+        ];
+    }
+
+    return [
+        'host' => '127.0.0.1',
+        'port' => DB_PORT,
+        'name' => 'admincorreos',
+        'user' => 'root',
+        'pass' => '',
+    ];
+}
 
 function getPdo(): PDO
 {
@@ -16,9 +36,11 @@ function getPdo(): PDO
         return $pdo;
     }
 
-    $dsn = sprintf('mysql:host=%s;port=%s;dbname=%s;charset=utf8mb4', DB_HOST, DB_PORT, DB_NAME);
+    $config = getDatabaseConfig();
 
-    $pdo = new PDO($dsn, DB_USER, DB_PASS, [
+    $dsn = sprintf('mysql:host=%s;port=%s;dbname=%s;charset=utf8mb4', $config['host'], $config['port'], $config['name']);
+
+    $pdo = new PDO($dsn, $config['user'], $config['pass'], [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);
